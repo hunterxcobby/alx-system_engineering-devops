4e939a1b0dd195b7a5aa9449ecd27fbcb246070c
hunterxcobby <solomonsefah13@gmail.com>
2024-01-30 00:19:54 +0000

    task 2: reloaded a 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,29 +1,16 @@
 # Use Puppet to automate the task of creating a custom HTTP header response
 
-# Update package information
-exec { 'apt-update':
+exec {'update':
   command => '/usr/bin/apt-get update',
 }
-
-# Install Nginx
-package { 'nginx':
-  ensure => 'installed',
+-> package {'nginx':
+  ensure => 'present',
 }
-
-# Configure Nginx with custom HTTP header
-file { '/etc/nginx/sites-available/default':
-  ensure => present,
-  content => "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\n\tserver_name _;\n\n\tlocation / {\n\t\tadd_header X-Served-By $hostname;\n\t\t# Your other configurations here\n\t}\n}",
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
-}
-
-# Enable and restart Nginx
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => [File['/etc/nginx/sites-available/default'], Package['nginx']],
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
